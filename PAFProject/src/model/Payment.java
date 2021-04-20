06913818b0ab4c182604b3d2b60f64e26a0e27f5
Sudarshana <Sudarshana@192.168.1.3>
2021-04-20 15:54:35 +0400

    insert validation

--- PAFProject/src/model/Payment.java
+++ PAFProject/src/model/Payment.java
@@ -1,10 +1,13 @@
 package model;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 public class Payment {
 	private Connection connect()
@@ -21,42 +24,86 @@ public class Payment {
 	 {e.printStackTrace();}
 	 return con;
 }
-
-public String insertPayment(String NIC, String creditNumber, String cvv, String expireDate,String date,String amount)
-{
-	String output = "";
-	try
-	{
-		Connection con = connect();
-		if (con == null)
-		{return "Error while connecting to the database for inserting."; }
-		// create a prepared statement
-		String query = " insert into payments(paymentID,NIC,creditNumber,cvv,expireDate,date,amount)"
-		+ " values (?, ?, ?, ?, ?, ?, ?)";
-		PreparedStatement preparedStmt = con.prepareStatement(query);
-		// binding values
-		preparedStmt.setInt(1, 0);
-		preparedStmt.setString(2,NIC);
-		preparedStmt.setString(3, creditNumber);
-		preparedStmt.setString(4, cvv);
-		preparedStmt.setString(5, expireDate);
-		preparedStmt.setString(6, date);
-		preparedStmt.setDouble(7, Double.parseDouble(amount));
+	public static boolean valDate(String date) {
+		DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
 		
-		//execute the statement
 		
-		preparedStmt.execute();
-		con.close();
-		output = "Inserted successfully";
+		df.setLenient(false);
+		
+		try {
+			
+			df.parse(date.trim());
+			
+			
+		}catch(Exception e) {
+			return false;
+		}
+		return true;
+		
+		
 	}
-	catch (Exception e)
-	{
-	output = "Error while inserting the item.";
-	System.err.println(e.getMessage());
+
+
+	public String insertPayment(String NIC, String creditNumber, String cvv, String expireDate,String date,String amount) {
+		String output = "";
+		try
+		 {
+		 Connection con = connect();
+		
+		 
+		 if (con == null)
+		 {return "Error while connecting to the database for inserting."; }
+		 else {
+			 try {
+				 
+				 Statement stmt = con.createStatement();
+				 String selectQuery = "SELECT * FROM payments WHERE creditNumber = '"+creditNumber+"'";
+				 
+				 ResultSet rs = stmt.executeQuery(selectQuery);
+				 
+				 if(rs.next()) {
+					 return "Already registerd same credit card number";
+				 }else if(NIC.equals("") || creditNumber.equals("") || cvv.equals("") || expireDate.equals("") || date.equals("") || amount.equals("")){
+					 return "Please fill all the details";
+				 }else if(valDate(expireDate) ==false){
+					 return "Expire Date should be 'dd-MM-yyyy' in format";
+				 }else if(valDate(date)== false){
+					 return "date should be 'dd-MM-yyyy' in format";
+				 }
+				 else {
+					 // create a prepared statement
+					 String query = " insert into payments(paymentID,NIC,creditNumber,cvv,expireDate,date,amount)"
+					 + " values (?, ?, ?, ?, ?, ?, ?)";
+					 PreparedStatement preparedStmt = con.prepareStatement(query);
+					 // binding values
+					 preparedStmt.setInt(1, 0);
+					 preparedStmt.setString(2, NIC);
+					 preparedStmt.setString(3, creditNumber);
+					 preparedStmt.setString(4, cvv);
+					 preparedStmt.setString(5, expireDate);
+					 preparedStmt.setString(6, date);
+					 preparedStmt.setDouble(7, Double.parseDouble(amount));		 
+					// execute the statement
+			
+					 preparedStmt.execute();
+					 con.close();
+					 output = "Inserted successfully";
+				 }
+		 }catch(Exception e) {
+			 
+		 }
+		 }
+		 }
+		 catch (Exception e)
+		 {
+		 output = "expire date should be greater than today date";
+		 System.err.println(e.getMessage());
+		 }
+		return output;
+	
+		
 	}
-	return output;
 }
+	
+	 
 
-
-
-}
